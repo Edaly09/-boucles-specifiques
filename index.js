fbdb26c1598b68291d8a8161aea64ac909d6ffe8
Edaly09 <edalyminthe@gmail.com>
2022-06-28 14:39:04 +0200

    filter exo

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
 // ## 01 - Map Double
 
-const array = [ 1, 2, 3, 4, 5]
+// const array = [ 1, 2, 3, 4, 5]
 
 
-const double = array.map((array) =>{
+// const double = array.map((array) =>{
 
-    console.log(array)
+//     console.log(array)
     
-    return array * 2
+//     return array * 2
 
-} )
+// } )
 
-console.log(array)
+// console.log(array)
 
 
-console.log(double)
+// console.log(double)
+
+// ## 02 - Map Names
+
+// const longNames = [{   firstName: "Jane",lastName: "Doe"},{firstName: "John",lastName: "Smith"} ]
+
+// const shortNames = longNames.map((longName) =>{
+
+//     return {
+
+//         name: "Jane Done"
+
+//     }
+
+//     return {
+        
+//         name: "John Smith"
+//     }
+       
+    
+// })
+
+//    console.log(shortNames)
+
+
+// ## 04 - Filter Even
+
+const numbers = [1,2,3,4,5,6,7,8]
+
+const even = numbers.filter((number) =>{
+
+    return number % 2 === 0
+})
+
+console.log(even)
+
 
