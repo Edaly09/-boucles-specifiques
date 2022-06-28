c321eae10d1602d922b17b2cdf61190c6904ea90
Edaly09 <edalyminthe@gmail.com>
2022-06-28 13:58:28 +0200

    exo sur map

---
+++ index.js
@@ -0,0 +1,18 @@
+// ## 01 - Map Double
+
+const array = [ 1, 2, 3, 4, 5]
+
+
+const double = array.map((array) =>{
+
+    console.log(array)
+    
+    return array * 2
+
+} )
+
+console.log(array)
+
+
+console.log(double)
+
