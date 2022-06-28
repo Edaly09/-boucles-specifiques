1b3b61130125cf4710fa1e962cd0f5874bd499a1
Edaly09 <edalyminthe@gmail.com>
2022-06-28 17:32:46 +0200

    exo 5

--- index.js
+++ index.js
@@ -54,8 +54,6 @@
 
 // console.log(numbers)
 
-
-
 // ## 04 - Filter Even
 
 // const numbers = [1,2,3,4,5,6,7,8]
@@ -68,3 +66,15 @@
 // console.log(even)
 
 
+// ## 05 - Cakes
+const cakes = [{name: "cake",flavor: "vanilla",status: "available"},{name: "brownie",flavor: "chocolate",status: "available"},{name: "pie",flavor: "strawberry",status: "available"},{name: "muffin",flavor: "pistachio",status: "available"},{name: "donut",flavor: "chocolate",status: "available"},]
+
+
+const cakeChocolate = cakes.filter((cake) =>{
+
+    
+})
+// - À l'aide des méthodes `.filter()` et `.map()`, vous donnerez le statut "sold out !" aux gâteaux au chocolat
+// - Ces deux gâteaux doivent être stockés dans une nouvelle variable, affichez-la dans la console
+
+// - Créez une variable `cakes` contenant la valeur suivante :
